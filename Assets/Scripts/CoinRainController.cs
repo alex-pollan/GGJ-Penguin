5fc6c2fe1f8050024699c1872fd568dffa5485b4
algorithmer <alexpollan@yahoo.com>
2015-01-24 14:20:52 +0000

    Start button and destroy coins when they falls

--- Assets/Scripts/CoinRainController.cs
+++ Assets/Scripts/CoinRainController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class CoinRainController : MonoBehaviour {
 
-	public Rigidbody2D coin;
 	public float timer = 1f;
 	private float timerBackup;
 
@@ -22,8 +21,13 @@ public class CoinRainController : MonoBehaviour {
 			Vector3 position = Random.value > 0.5 
 				? new Vector3(Random.Range(-4.5f, -1.0f), 10.0f ,0.0f)
 					: new Vector3(Random.Range(1.0f, 4.5f), 10.0f, 0.0f);
-			
-			Instantiate (coin, position, Quaternion.identity);
+
+			GameObject coin = GameObject.Find("Coin");
+			GameObject clone = Instantiate (coin, position, Quaternion.identity) as GameObject;
+			clone.SetActive(true);
+			clone.rigidbody2D.isKinematic = false;
+			GameObject game = GameObject.Find("Game");
+			clone.transform.parent = game.transform;
 		}
 
 	}
