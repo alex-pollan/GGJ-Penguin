48663858a5847e9293be386ed2e8a06d87805116
algorithmer <alexpollan@yahoo.com>
2015-01-24 10:55:16 +0000

    first commit

---
+++ Assets/Scripts/CoinRainController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinRainController : MonoBehaviour {
+
+	public Rigidbody2D coin;
+	public float timer = 1f;
+	private float timerBackup;
+
+	void Start()
+	{
+		timerBackup = timer;
+	}
+
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if (timer <= 0) 
+		{
+			timer = timerBackup;
+			
+			Vector3 position = Random.value > 0.5 
+				? new Vector3(Random.Range(-4.5f, -1.0f), 10.0f ,0.0f)
+					: new Vector3(Random.Range(1.0f, 4.5f), 10.0f, 0.0f);
+			
+			Instantiate (coin, position, Quaternion.identity);
+		}
+
+	}
+}
